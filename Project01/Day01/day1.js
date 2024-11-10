3e35f911decea3b260181e8f5339cdb534e31c38
Lim <3269989624@qq.com>
2024-11-10 21:42:06 +0800

    24.11.10 :tada:

---
+++ Project01/Day01/day1.js
@@ -0,0 +1,9 @@
+//显示转换
+let a = Number(prompt("输入a"));
+console.log((typeof a) + a);
+
+//隐式转换
+let b = +prompt("输入b");
+console.log(typeof (b) + b);
+
+
---
+++ Project01/Function/Test/test01.js
@@ -0,0 +1,7 @@
+'use strict'
+let num = 20
+function fun1(){
+  num = 10
+}
+fun1()
+console.log(num);
